5982218f89fab666c9aed31a138eea2dade5d4be
sGeeK <francois_perin@hotmail.fr>
2016-02-10 23:09:10 +0100

    Add title

--- src/app/lucca-chat.ts
+++ src/app/lucca-chat.ts
@@ -1,17 +1,16 @@
 import {Component} from 'angular2/core';
-
+import {Title} from 'angular2/platform/browser';
 
 @Component({
   selector: 'lucca-chat-app',
+	viewProviders: [Title],
   providers: [],
   templateUrl: 'app/lucca-chat.html',
   directives: [],
   pipes: []
 })
-export class LuccaChatApp {
-  defaultMeaning: number = 42;
-  
-  meaningOfLife(meaning) {
-    return `The meaning of life is ${meaning || this.defaultMeaning}`;
+export class LuccaChatApp {  
+	constructor(title: Title) {
+		title.setTitle('Lucca chat');
   }
 }
