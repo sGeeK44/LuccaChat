f377c4582aea1ed7cdb692ccf4e78cc66e2bbba4
sGeeK <francois_perin@hotmail.fr>
2016-02-19 09:48:52 +0100

    Add event on keyup.enter on input chat bar

--- src/app/input-chat-bar.ts
+++ src/app/input-chat-bar.ts
@@ -6,8 +6,8 @@ import {EventEmitter} from 'angular2/core';
 	selector: 'lc-input-chat-bar',
 	styles: [],
 	template:
-	`<div id='inputChatBar' class=input-chat-bar>
-		<input type="text" style='width:80%' placeholder="Votre message..." autofocus [(ngModel)]="inputValue" #inputMessage/>
+	`<div id='inputChatBar' class='input-chat-bar'>
+		<input type="text" (keyup.enter)="SendNewMessage()" style='width:80%' placeholder="Votre message..." autofocus [(ngModel)]="inputValue" #inputMessage/>
 		<input type='button' style='margin-left:10px;' (click)='SendNewMessage();inputMessage.focus()' value='Envoyer'/>
 	</div>`
 })
