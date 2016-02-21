132ee67c664f3062caa1e3ebabc14426d6da4860
sGeeK <francois_perin@hotmail.fr>
2016-02-21 11:52:13 +0100

    Add guid id to manage same pseudo

--- src/app/identity.ts
+++ src/app/identity.ts
@@ -3,6 +3,7 @@ import {Injectable} from "angular2/core";
 @Injectable()
 export class Identity {
     private static DEFAULT_NICKNAME: string = "Anonyme";
+    public id : string = Identity.NewGuid();
     public nickName: string;
   
     public constructor() {
@@ -21,5 +22,13 @@ export class Identity {
     private NickNameIsNotSet() : boolean
     {
         return this.nickName == '' || this.nickName == null;
+    }
+    
+    private static NewGuid() : string
+    {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+            return v.toString(16);
+        });
     }
 }
