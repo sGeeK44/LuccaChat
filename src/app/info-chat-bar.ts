27271c2b00bafc64b27984b8e0d01951b19695bb
sGeeK <francois_perin@hotmail.fr>
2016-02-21 12:13:29 +0100

    Inform user when other writting message

--- src/app/info-chat-bar.ts
+++ src/app/info-chat-bar.ts
@@ -1,42 +1,103 @@
 import {Component} from 'angular2/core';
 import {Message} from './message';
 import {Bus, BusObserver, MessageBus} from './message-bus';
+import {Identity} from './identity';
 
 @Component({
 	selector: 'lc-info-chat-bar',
 	styles: [ ],
-	template: `<p>&nbsp;{{info}}</p>`
+	template: 
+        `<p>&nbsp;
+            <span *ngIf='writterList.length == 0'>{{info}}</span>
+            <em *ngIf='writterList.length == 1'>{{writterList[0].identity.nickName}} écrit un message...</em>
+            <em *ngIf='writterList.length > 1'>Plusieurs personnes écrivent un message...</em>
+        </p>`
 })
 export class InfoChatBar implements BusObserver {
     private info: string;
+    private writterList: Array<Writter> = [];
     
     constructor(private bus: Bus)
     {
         bus.Subsribe(this);
-    }
-    
-	public DisplayNewConnection(nickName: string) : void {   
-		this.Display(nickName + ' vient de se connecter');
-	}
-    
-    public Display(message: string)
-    {
-        this.SetInfo(message);
-        setTimeout(() => {this.CleanInfo();}, 5000);
-    }
-    
-    public SetInfo(message : string) : void
-    {
-        this.info = message;        
-    }
-    
-    public CleanInfo() : void
-    {
-        this.info = null;
+        this.CleanObsoleteWritterEachHalfSecond();
     }
     
     public Consume(message: MessageBus) : void
     {
         if (message.topic == 'Connect') this.DisplayNewConnection(message.content);
+        if (message.topic == 'Typing') this.AddWritter(Identity.Deserialize(message.content));
     }
+    
+	private DisplayNewConnection(nickName: string) : void {   
+		this.Display(nickName + ' vient de se connecter');
+	}
+    
+    private Display(message: string)
+    {
+        this.SetInfo(message);
+        this.ClearInfoAfterThreeSecond();
+    }
+    
+    private SetInfo(message : string) : void
+    {
+        this.info = message;        
+    }
+    
+    private ClearInfoAfterThreeSecond() : void
+    {
+        setTimeout(() => { this.CleanInfo(); }, 3000);
+    }
+    
+    private CleanInfo() : void
+    {
+        this.info = null;
+    }
+    
+    private AddWritter(writterIdentity: Identity) : void
+    {
+        let notFound = -1;
+        let index = this.FindIndexWritter(writterIdentity);
+        if (index != notFound) this.writterList[index].lastTyping = new Date();
+        else this.writterList.push(new Writter(writterIdentity));
+    }
+    
+    private FindIndexWritter(writterIdentity: Identity) : number
+    {
+        return this.writterList.findIndex((x) => { return x.identity.id == writterIdentity.id; });
+    }
+    
+    private CleanObsoleteWritterEachHalfSecond() : void
+    {
+        setInterval(() => { this.CleanObsoleteWritter(); }, 500);
+    }
+    
+    private CleanObsoleteWritter() : void
+    {
+        let oneSecond = 1000;
+        this.writterList.forEach(writter => {
+            let lastTyping = this.GetElapseTimeFromLastTyping(writter);
+            if (lastTyping > oneSecond) this.RemoveWritter(writter);            
+        });
+    }
+    
+    private GetElapseTimeFromLastTyping(writter: Writter) : number
+    {
+        return new Date().valueOf() - writter.lastTyping.valueOf();
+    }
+    
+    private RemoveWritter(writter: Writter)
+    {
+        let indexElementToRemove = this.writterList.indexOf(writter);
+        if (indexElementToRemove == -1) return;
+         this.writterList.splice(indexElementToRemove, 1);        
+    }
+}
+
+class Writter {
+    public lastTyping: Date;
+    
+    constructor(public identity: Identity) {
+        this.lastTyping = new Date();
+     }
 }
--- src/app/input-chat-bar.ts
+++ src/app/input-chat-bar.ts
@@ -1,4 +1,5 @@
 import {Component} from 'angular2/core';
+import {Control} from 'angular2/common';
 import {Output} from 'angular2/core';
 import {EventEmitter} from 'angular2/core';
 import {Bus, MessageBus} from './message-bus';
@@ -10,20 +11,35 @@ import {Message} from './message';
 	styles: [],
 	template:
 	`<div class='input-chat-bar'>
-		<input type="text" (keyup.enter)="SendNewMessage()" style='width:80%' placeholder="Votre message..." autofocus [(ngModel)]="inputValue" #inputMessage/>
+		<input type="text" (keyup.enter)="SendNewMessage()" style='width:80%' placeholder="Votre message..." autofocus [ngFormControl]='input' [(ngModel)]="inputValue" #inputMessage/>
 		<input type='button' style='margin-left:10px;' (click)='SendNewMessage();inputMessage.focus()' value='Envoyer'/>
 	</div>`
 })
 export class InputChatBar {
 	private inputValue : string = '';
+    private input = new Control();
     
 	@Output() public newMessageSended: EventEmitter<string> = new EventEmitter<string>();
     
-    constructor(private bus: Bus, private identity: Identity) { }
+    constructor(private bus: Bus, private identity: Identity) {
+        this.InformWhenTypingMessage()
+     }
 	
 	public SendNewMessage() : void {
 		this.newMessageSended.emit(this.inputValue);
         this.bus.Publish(new MessageBus('Send', (new Message(this.identity, this.inputValue)).Serialize()));
-        this.inputValue = '';
+        this.ClearInputValue();
 	}
+    
+    private InformWhenTypingMessage() : void
+    {
+        this.input.valueChanges.subscribe(input => {
+            this.bus.Publish(new MessageBus('Typing', this.identity.Serialize()));
+        });
+    }
+    
+    private ClearInputValue() : void
+    {
+        this.inputValue = '';
+    }
 }
